960302560bedc20fb1a1ec151582956a0c588042
Andrew Fenner <afenner_ic@mindresearch.org>
2019-10-24 17:12:19 -0400

    arena commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Arena from "./Arena"
+import * as Pixi from "pixi.js";
+//import * as HundredsArrayScript from "./js/hundredsarray.js"
+import * as NumberLineStripsScript from "./js/numberlinestrips.js"
+import * as CapacityTalkData from "./Activities/CapacityTalk.json";
+
+Pixi.settings.RESOLUTION = 3
+let app = new Pixi.Application(0,0,{backgroundColor: 0xffffff,antialias: true});
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p> Here's a paragraph</p>
-      </header>
+    <div >
+      <Arena app = {app} fullscreen = {true} lesson = {CapacityTalkData.default} script = {NumberLineStripsScript.init}/>
     </div>
   );
 }
---
+++ src/js/const.js
@@ -0,0 +1,83 @@
+import blue_gradient from "../assets/blue-gradient.png";
+import plus from "../assets/Plus.png";
+import reset from "../assets/Reset.png"
+import trash from "../assets/Trash.png"
+import jiji from "../assets/jiji.png"
+import minus from "../assets/Minus.png"
+import blue_circle from "../assets/BlueCircle.png"
+import red_circle from "../assets/RedCircle.png"
+import empty_circle from "../assets/EmptyCircle.png"
+import plus_ten from "../assets/PlusTen.png"
+import minus_ten from "../assets/MinusTen.png"
+import plus_one from "../assets/PlusOne.png"
+import minus_one from "../assets/MinusOne.png"
+import unlocked_lock  from "../assets/UnlockedLock.png"
+import locked_lock from "../assets/LockedLock.png"
+import sharp_pin from "../assets/SharpPin.png"
+import arrow from "../assets/Arrow.png"
+
+
+export const ASSETS = {
+    BLUE_GRADIENT: blue_gradient,
+    PLUS: plus,
+    RESET: reset,
+    TRASH: trash,
+    JIJI: jiji,
+    MINUS: minus,
+    BLUE_CIRCLE: blue_circle,
+    RED_CIRCLE: red_circle,
+    EMPTY_CIRCLE: empty_circle,
+    PLUS_TEN: plus_ten,
+    MINUS_TEN: minus_ten,
+    PLUS_ONE: plus_one,
+    MINUS_ONE: minus_one,
+    UNLOCKED_LOCK: unlocked_lock,
+    LOCKED_LOCK: locked_lock,
+    SHARP_PIN: sharp_pin,
+    ARROW: arrow
+}
+
+export const CUISENAIRE_COLORS = {
+  "1": 0xfff9fc,
+  "2": 0xff385c,
+  "3": 0x77e060,
+  "4": 0x781fb7,
+  "5": 0xf9f32f,
+  "6": 0x0cad85,
+  "7": 0x202628,
+  "8": 0xb24118,
+  "9": 0x1e3484,
+  "10": 0xf77613,
+  "11": 0x8b959b,
+  "12": 0xff77e8
+};
+
+export const COLORS = {
+    ORANGE: 0xffaa32,
+    CORAL: 0xff708a,
+    RED: 0xff5121,
+    PURPLE: 0x9966ff,
+    BLUE: 0x216ae0,
+    PINK: 0xf721ff,
+    GREEN: 0x50af54,
+    ORANGE: 0xffaa49,
+    BROWN: 0x82521b,
+    GRAY: 0xe6eaf2,
+    DARK_GRAY: 0xa4a9b2
+  };
+
+export const FRACTION_BAR_COLORS = {
+  "1": 0xfff9fc,
+  "2": 0xff385c,
+  "3": 0x77e060,
+  "4": 0xc063ff,
+  "5": 0xf9f32f,
+  "6": 0x0cad85,
+  "7": COLORS.BLUE,
+  "8": 0xb24118,
+  "9": COLORS.RED,
+  "10": 0xf77613,
+  "11": 0x8b959b,
+  "12": 0xff77e8
+};
+  
