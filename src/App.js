13cb816be4c221444f408ed56addeac566a21a7c
Andrew Fenner <afenner_ic@mindresearch.org>
2019-10-24 17:21:43 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -3,18 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 import Arena from "./Arena"
 import * as Pixi from "pixi.js";
-//import * as HundredsArrayScript from "./js/hundredsarray.js"
+import { BrowserRouter, Switch, Route, Link } from "react-router-dom";
+import * as HundredsArrayScript from "./js/hundredsarray.js"
 import * as NumberLineStripsScript from "./js/numberlinestrips.js"
 import * as CapacityTalkData from "./Activities/CapacityTalk.json";
 
 Pixi.settings.RESOLUTION = 3
 let app = new Pixi.Application(0,0,{backgroundColor: 0xffffff,antialias: true});
 
+const Main = () => (
+  <div>
+    <Route exact path="/" component={() => <Arena app = {app} fullscreen = {true} lesson = {CapacityTalkData.default} script = {HundredsArrayScript.init}/>} />
+    <Route exact path="/strips" component={() => <Arena app = {app} fullscreen = {true} lesson = {CapacityTalkData.default} script = {NumberLineStripsScript.init}/>} />
+  </div>
+);
 
 function App() {
   return (
     <div >
-      <Arena app = {app} fullscreen = {true} lesson = {CapacityTalkData.default} script = {NumberLineStripsScript.init}/>
+       <BrowserRouter className="App-container">
+          <Main />
+        </BrowserRouter>
     </div>
   );
 }
--- src/Arena.js
+++ src/Arena.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import { Button, Icon, Navbar, NavItem } from "react-materialize";
-import Modal from '@material-ui/core/Modal';
 import Paper from '@material-ui/core/Paper';
 import Drawer from '@material-ui/core/Drawer';
 import { TweenMax, TimelineLite, Power2, Elastic, CSSPlugin, TweenLite, TimelineMax } from "gsap/TweenMax";
