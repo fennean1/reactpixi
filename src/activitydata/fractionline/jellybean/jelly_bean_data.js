4bf086f3cc57626c28a507055af1c4c068e64bd5
Andrew Fenner <afenner_ic@mindresearch.org>
2019-10-31 13:23:54 -0400

    silky smooze

--- src/activitydata/fractionline/jellybean/jelly_bean_data.js
+++ src/activitydata/fractionline/jellybean/jelly_bean_data.js
@@ -17,7 +17,7 @@ export const ACTIVITY = {
     DESCRIPTION: 'Students have to use fractional parts to optimize the amount of jelly beans they can buy. For example  "Should I get 1/3 of 30 or 1/2 of 24?"',
     LONG_DESCRIPTION: "Here is a very long description of what is happeneing in the activity I mean it's so long don't you think? Here is a very long description of what is happeneing in the activity I mean it's so long don't you think? Here is a very long description of what is happeneing in the activity I mean it's so long don't you think?",
     INSTRUCTIONS: 'Students should...',
-    STANDARD_ID: "4.NF.B.4.A",
+    STANDARD_ID: "555",
     STANDARD_DESCRIPTION: "Hello",
     TAGS: ["Place Value","Addition","First Grade"],
     LEARNING_GOAL: "goal for learning",
