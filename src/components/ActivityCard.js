58fb107b0417220082e76d3d81945517fc330c5d
Andrew Fenner <fennean1@gmail.com>
2020-01-22 09:08:18 -0500

    icon removed

--- src/components/ActivityCard.js
+++ src/components/ActivityCard.js
@@ -73,7 +73,7 @@ export default function ActivityCard(props) {
       <img className="activator" src={props.data.ICON}/>
     </div>
     <div className="card-content">
-      <span className="card-title activator grey-text text-darken-4">{props.data.TITLE}<i className="material-icons right">more_vert</i></span>
+      <span className="card-title activator grey-text text-darken-4">{props.data.TITLE}</span>
       <p>{props.data.TIME+ " Minutes"}</p>
     </div>
     <div className="card-action"><Link  to={{pathname: `${activitypath}`, state: {data: props.data}}}>
